487e33c1e5240c9f51cbcbeb58e4725cf112850d
DConnell1011100 <56926063+DConnell1011100@users.noreply.github.com>
2020-02-04 10:09:21 +0000

    Add verification to user input
    Cannot add numbers outside of 1-40 range, calling methods to stop duplicate input - still need to build the method within the instantiable class.

--- LotteryApp/src/lotteryapp/Lottery.java
+++ LotteryApp/src/lotteryapp/Lottery.java
@@ -13,28 +13,25 @@ public class Lottery {
     private int[][] userNumbers;
     private int noOfLines;
     private boolean withinRange;
-    private boolean userNumberDuplicate;
-    
+    private boolean userNumberDuplicate = false;
+
     public void setNoOfLines(int noOfLines) {
-        this.noOfLines = noOfLines;
-    }
-    
-    public void setUserNumbers(int number, int currentLineNumber) {
-        userNumbers = new int[noOfLines][6];
-        userNumbers[currentLineNumber][0] = number;
-        for(int i = 1; i < 6; i++){
-            for(int j = 0; j <= i; j++){
-                if(number != userNumbers[currentLineNumber][j]){
-                    userNumbers[currentLineNumber][i] = number;
-                    userNumberDuplicate = false;
-                }
-                else{
-                    i--;
-                    userNumberDuplicate = true;
-                }
-            }
+        if (noOfLines >= 1 && noOfLines <= 3) {
+            this.noOfLines = noOfLines;
+            withinRange = true;
+        } else {
+            System.out.println("Please enter a number between 1-3");
+            withinRange = false;
         }
-       
+    }
+
+    public void setUserNumbers(int number) {
+        userNumbers = new int[noOfLines][6];
+
+    }
+
+    public void withinRange(int number) {
+        withinRange = number >= 1 && number <= 40;
     }
 
     public void createSecretNumbers() {
@@ -43,39 +40,26 @@ public class Lottery {
         int n = myRan.nextInt(40);
         n += 1;
         secretNumbers[0] = n;
-        for(int i = 1; i < 6;i ++){
+        for (int i = 1; i < 6; i++) {
             n = myRan.nextInt(40);
             n += 1;
-            for(int j = 0; j < i; j++){
-                if(n != secretNumbers[j]){
+            for (int j = 0; j < i; j++) {
+                if (n != secretNumbers[j]) {
                     secretNumbers[i] = n;
-                }
-                else{
+                } else {
                     i--;
                 }
             }
         }
-            
-    }
-     
 
-
-    public void withinRange(int number, int low, int high){
-        if (number >= low && number <= high) {
-            withinRange = true;
-        } else {
-            withinRange = false;
-            System.out.println("That is not a number between " + low + " and " + high);
-        }
     }
-    
-    public void eliminateDuplicateUserInput(int currentLinePosition, int currentNumberPosition, int currentNumberValue){
-        for(int i = 0; i < currentLinePosition; i++){
-            for(int j = 0; i < currentNumberPosition; i++){
-                if(currentNumberValue != userNumbers[i][j]){
+
+    public void checkDuplicateInput(int currentLinePosition, int currentNumberPosition, int currentNumberValue) {
+        for (int i = 0; i < currentLinePosition; i++) {
+            for (int j = 0; i < currentNumberPosition; i++) {
+                if (currentNumberValue != userNumbers[i][j]) {
                     userNumberDuplicate = false;
-                }
-                else{
+                } else {
                     userNumberDuplicate = true;
                 }
             }
@@ -97,9 +81,9 @@ public class Lottery {
     public int[] getSecretNumbers() {
         return secretNumbers;
     }
-    
-    public boolean getUserNumberDuplicate(){
+
+    public boolean getUserNumberDuplicate() {
         return userNumberDuplicate;
     }
-    
+
 }
--- LotteryApp/src/lotteryapp/LotteryGame.java
+++ LotteryApp/src/lotteryapp/LotteryGame.java
@@ -11,11 +11,10 @@ public class LotteryGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int [] secretNumbers;
+        //int [] secretNumbers;
         int [][] userNumbers;
-        boolean withinRange;
-        int n;
-        boolean userNumberDuplicate;
+        boolean withinRange = false;
+        boolean userNumberDuplicate = true;
         
         
         Scanner input = new Scanner(System.in);
@@ -25,42 +24,73 @@ public class LotteryGame {
         //ask home many lines the user wants to play
         do{
             System.out.println("How many lines would you like to play?");
-            n = input.nextInt();
-            myLott.withinRange(n, 1, 3);
+            myLott.setNoOfLines(input.nextInt());
             withinRange = myLott.getWithinRange();
         }
         while(withinRange == false);
         
-        myLott.setNoOfLines(n);
      
-        
+        //initialize user's input with number of lines (rows) and 6 numbers per line(column)
         userNumbers = new int[myLott.getNoOfLines()][6];
+        
+        /*for testing purposes only - displays secret nuymb
         myLott.createSecretNumbers();
         secretNumbers = myLott.getSecretNumbers();
+        
         
         for(int i = 0; i < 6; i++){
             System.out.print(secretNumbers[i]+" ");
         }
         System.out.println("");
-        
+        */
+        //get user input 
         for(int i = 0; i < userNumbers.length; i++){
             for(int j = 0; j < userNumbers[i].length; j++){
-                int currentLineNumber = i;
-                System.out.println("Entry for line " + (i+1) + " number " +(j+1) + ": ");
-                myLott.withinRange(input.nextInt(), 1, 40);
-                withinRange = myLott.getWithinRange();
-                if(withinRange == true){
-                    myLott.setUserNumbers(userNumbers[i][j], currentLineNumber);
-                    userNumberDuplicate = myLott.getUserNumberDuplicate();
-                    if(userNumberDuplicate = true){
-                        System.out.println("Cannot enter duplicate numbers");
-                        j--;
+                //declare a variable to store the currentNumber
+                int currentNumber;
+                //ask the user input
+                do{
+                    System.out.println("Entry for line " + (i+1) + " number " +(j+1) + ": ");
+                    //store the input in currentVariable
+                    currentNumber = input.nextInt();
+                    //check if number is within range
+                    myLott.withinRange(currentNumber);
+                    //get the result of that check
+                    withinRange = myLott.getWithinRange();
+                    //if withinRange if false, print out of range number
+                    if(!withinRange){
+                        System.out.println("Not a number between 1-40");
                     }
-                }
-                else{
-                    System.out.println("That is not a number between 1-40");
-                    j--;
-                }
+                    //if it is within range, continue the verification process
+                    else if(withinRange){
+                        //call method to check if number has already been entered by passing in i(current line), j(current number position), and currentNumber
+                        myLott.checkDuplicateInput(i, j, currentNumber);
+                        //set the boolean by calling the result from the IC
+                        userNumberDuplicate = myLott.getUserNumberDuplicate();
+                        //if it is true, then tell user and repeat processs
+                        if(userNumberDuplicate){
+                            System.out.println("You cannot enter duplicate numbers in a line");
+                        }
+                        //if it is not a duplicate, end the do while and let the normal for loops increment
+                        else if(!userNumberDuplicate){
+                            //set user numbers here, and set them in the IC in case we need to call them later
+                            userNumbers[i][j] = currentNumber;
+                            myLott.setUserNumbers(currentNumber);
+                        }
+                    }
+                    //do while loop when the number is not in range
+                }while(withinRange == false || userNumberDuplicate == true);
+                
+                    
+                
+                
+               
+            }
+        }
+        
+        for(int i = 0; i < userNumbers.length; i++){
+            for(int j = 0; j < 6; j++){
+                System.out.println(userNumbers[i][j]);
             }
         }
     }
