37661ce707dc113e19e43dbb679db04a0837ac0b
DConnell1011100 <DConnell1011100@users.noreply.github.com>
2020-02-13 11:31:49 +0000

    Added play again functionality
    Added a do while loop to check if the player wants to play again or not - will restart the game if enter yes or y - otherwise it will exit

--- LotteryApp/src/lotteryapp/LotteryGame.java
+++ LotteryApp/src/lotteryapp/LotteryGame.java
@@ -20,99 +20,105 @@ public class LotteryGame {
         int invalidEntry;
         int currentLineNo;
         int currentNumber;
+        boolean playAgain;
         
         
         Scanner input = new Scanner(System.in);
         
         Lottery myLott = new Lottery();
-        
+        do{
         //ask how many lines the user wants to play
         
-        do{
-            System.out.println("How many lines would you like to play?");
-            noOfLines = input.nextInt();
-            myLott.setNoOfLines(noOfLines);
-            withinRange = myLott.getWithinRange();
-        }
-        while(withinRange == false);
-        
-        
-        myLott.createSecretNumbers();
-        secretNumbers = myLott.getSecretNumbers();
-
-        for(int i = 0; i < secretNumbers.length; i++){          
-            System.out.print(myLott.getSecretNumbers()[i]+ " ");
-        }
-        
-     
-        //initialize user's input with number of lines (rows) and 6 numbers per line(column)
-        userNumbers = new int[noOfLines][6];
-        //get user input 
-        //set the current line number to 0 to start.
-        currentLineNo = 0;
-        do{
             do{
-            System.out.println("Entry for line " + (currentLineNo+1) + " number 1: ");
-            currentNumber = input.nextInt();
-            myLott.withinRange(currentNumber);
-            withinRange = myLott.getWithinRange();
-            if(withinRange == true){
-                userNumbers[currentLineNo][0] = currentNumber;
+                System.out.println("How many lines would you like to play?");
+                noOfLines = input.nextInt();
+                myLott.setNoOfLines(noOfLines);
+                withinRange = myLott.getWithinRange();
             }
-            else{
-                System.out.println("Not a number between 1-40");
+            while(withinRange == false);
+
+
+            myLott.createSecretNumbers();
+            secretNumbers = myLott.getSecretNumbers();
+
+            for(int i = 0; i < secretNumbers.length; i++){          
+                System.out.print(myLott.getSecretNumbers()[i]+ " ");
             }
-            }while(withinRange == false);
-            for(int i = 1; i < 6;){
-                invalidEntry = 1;
-                //ask the user input
-                System.out.println("Entry for line " + (currentLineNo+1) + " number " +(i+1) + ": ");
+
+
+            //initialize user's input with number of lines (rows) and 6 numbers per line(column)
+            userNumbers = new int[noOfLines][6];
+            //get user input 
+            //set the current line number to 0 to start.
+            currentLineNo = 0;
+            do{
+                do{
+                System.out.println("Entry for line " + (currentLineNo+1) + " number 1: ");
                 currentNumber = input.nextInt();
-                for(int j = 0; j < i; j++){
-                    //check if number is within range
-                    myLott.withinRange(currentNumber);
-                    //get the result of that check
-                    withinRange = myLott.getWithinRange();
-                    //if withinRange if false, print out of range number
-                    if(withinRange == false){
-                        System.out.println("Not a number between 1-40");
-                        invalidEntry = 0;
-                        break;
-                    }
-                    else if(withinRange == true){
-                        if(currentNumber == userNumbers[currentLineNo][j]){
+                myLott.withinRange(currentNumber);
+                withinRange = myLott.getWithinRange();
+                if(withinRange == true){
+                    userNumbers[currentLineNo][0] = currentNumber;
+                }
+                else{
+                    System.out.println("Not a number between 1-40");
+                }
+                }while(withinRange == false);
+                for(int i = 1; i < 6;){
+                    invalidEntry = 1;
+                    //ask the user input
+                    System.out.println("Entry for line " + (currentLineNo+1) + " number " +(i+1) + ": ");
+                    currentNumber = input.nextInt();
+                    for(int j = 0; j < i; j++){
+                        //check if number is within range
+                        myLott.withinRange(currentNumber);
+                        //get the result of that check
+                        withinRange = myLott.getWithinRange();
+                        //if withinRange if false, print out of range number
+                        if(withinRange == false){
+                            System.out.println("Not a number between 1-40");
                             invalidEntry = 0;
-                            System.out.println("Duplicate Number");
                             break;
                         }
+                        else if(withinRange == true){
+                            if(currentNumber == userNumbers[currentLineNo][j]){
+                                invalidEntry = 0;
+                                System.out.println("Duplicate Number");
+                                break;
+                            }
+                        }
+                    }//end of j loop
+                    if(invalidEntry == 1){
+                        userNumbers[currentLineNo][i] = currentNumber;
+                        i++;
                     }
-                }//end of j loop
-                if(invalidEntry == 1){
-                    userNumbers[currentLineNo][i] = currentNumber;
-                    i++;
+                }//end of i loop
+                currentLineNo++;
+            }while(currentLineNo < myLott.getNoOfLines());//stop repeating when all line numbers are filled
+
+            myLott.setUserNumbers(userNumbers);
+
+
+            myLott.checkMatches();
+
+            matches = myLott.getMatches();
+
+            for(int i = 0; i < matches.length; i++){
+                for(int j = 0; j < matches[i].length; j++){
+                    System.out.println("Matches on line " + (i+1) + ": " + myLott.getMatches()[i][j]);
                 }
-            }//end of i loop
-            currentLineNo++;
-        }while(currentLineNo < myLott.getNoOfLines());//stop repeating when all line numbers are filled
-        
-        myLott.setUserNumbers(userNumbers);
-        
-            
-        myLott.checkMatches();
-        
-        matches = myLott.getMatches();
-        
-        for(int i = 0; i < matches.length; i++){
-            for(int j = 0; j < matches[i].length; j++){
-                System.out.println("Matches on line " + (i+1) + ": " + myLott.getMatches()[i][j]);
             }
-        }
-        for(int i = 0; i < userNumbers.length; i++){
-            System.out.println(" ");
-            for(int j = 0; j < userNumbers[i].length; j++){
-                System.out.print(myLott.getUserNumbers()[i][j]+ " ");
+            System.out.println("Do you want to play again?");
+            input.nextLine();//fix for the nextInt scanner issue
+            String play = input.nextLine();
+            if(play.equalsIgnoreCase("Yes") || play.equalsIgnoreCase("Y")){
+                playAgain = true;
             }
-        }
+            else{
+                System.out.println("Thank you for playing! Game over.");
+                playAgain = false;
+            }
+        }while(playAgain == true);
     }
     
 }
